a6d947cd05df00a36f9625edc5dfe581cfc2b0df
hyuan1317 <kool717alex@gmail.com>
2022-02-21 12:28:23 +1100

    feat(CategoryShows): import CategoryShowCard and adds title

--- src/page-components/Category/components/CategoryShows/index.js
+++ src/page-components/Category/components/CategoryShows/index.js
@@ -2,34 +2,57 @@ import { Flex } from '@rebass/grid';
 import Paragraph from 'shared-components/Typography/Paragraph';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { StyledBox, StyledCategoryShows, TextWrapper } from './styled';
+import {
+  StyledBox,
+  StyledCategoryShows,
+  TextWrapper,
+  ShowsWrapper,
+  TitleWrapper,
+} from './styled';
+import CategoryShowCard from '../CategoryShowCard';
 
 function CategoryShows({ shows, description }) {
+  const showsNumber = shows.length;
+
   return (
     <StyledCategoryShows>
+      <TitleWrapper>
+        <Paragraph
+          text={`${showsNumber} Podcasts`}
+          variant="xl"
+          fontWeight={700}
+        />
+      </TitleWrapper>
       <Flex justifyContent="space-between" alignItems="center" flexWrap="wrap">
         <StyledBox>
           {description && (
-          <TextWrapper>
-            <Paragraph text={description} variant="l" transparent />
-          </TextWrapper>
+            <TextWrapper>
+              <Paragraph text={description} variant="l" transparent />
+            </TextWrapper>
           )}
         </StyledBox>
       </Flex>
+      <ShowsWrapper>
+        {shows.map((show) => (
+          <CategoryShowCard key={show.name} show={show} />
+        ))}
+      </ShowsWrapper>
     </StyledCategoryShows>
   );
 }
 
 CategoryShows.propTypes = {
-  shows: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string,
-    description: PropTypes.string,
-    images: PropTypes.shape({
-      squareLarge: PropTypes.shape({
-        url: PropTypes.string,
+  shows: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+      images: PropTypes.shape({
+        squareLarge: PropTypes.shape({
+          url: PropTypes.string,
+        }),
       }),
-    }),
-  })),
+    })
+  ),
   description: PropTypes.string,
 };
 
--- src/page-components/Category/components/CategoryShows/styled.js
+++ src/page-components/Category/components/CategoryShows/styled.js
@@ -4,17 +4,17 @@ import spacing from 'styling/spacing';
 import screen from 'styling/screen';
 
 export const StyledCategoryShows = styled(Box)`
-   background-color: ${props => props.theme.dark};
-   border-radius: 26px;
-   padding: ${spacing.m};
+  background-color: ${(props) => props.theme.dark};
+  border-radius: 26px;
+  padding: ${spacing.m};
 
-   ${screen.tablet} {
+  ${screen.tablet} {
     padding: 15px;
   }
 `;
 
 export const StyledBox = styled(Box)`
-   max-width: 100%;
+  max-width: 100%;
 
   ${screen.mobile} {
     max-width: 480px;
@@ -25,9 +25,32 @@ export const StyledBox = styled(Box)`
   }
 `;
 export const TextWrapper = styled.div`
-   margin-bottom: 12px;
+  margin-bottom: 12px;
 
-   ${screen.mobile} {
+  ${screen.mobile} {
     margin-bottom: 0;
+  }
+`;
+export const TitleWrapper = styled.div`
+  margin: 10px 0 30px 0;
+`;
+
+export const ShowsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 15px;
+  grid-row-gap: 40px;
+
+  ${screen.mobile} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  ${screen.tablet} {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  ${screen.laptop} {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  ${screen.desktop} {
+    grid-template-columns: repeat(5, 1fr);
   }
 `;
